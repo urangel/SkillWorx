8fa0ebec1d1ab2d2e8e5395d8e0c3d1d76867c97
Ulises <uli.atx@gmail.com>
2019-06-01 09:06:42 -0500

    image compress and scale, dropdown nav moved

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,14 +5,14 @@ import DropDown from '../NavbarDropDown/index'
 function Navbar() {
   return (
     <div id="fixedNavbar">
-    <nav className="navbar navbar-light notLanding" >
+    <nav className="navbar navbar-light notLanding d-flex justify-content-around" >
       <span className="navbar-brand mb-0">
         <span className="cap-brand">S</span>
         <span className="small-cap-brand kill">kill</span>
         <span className="cap-brand w">W</span>
         <span className="small-cap-brand">orx</span>
       </span>
-      <div className="d-flex mr-5" id="dropDownMedia">
+      <div id="dropDownMedia">
      <DropDown/>
       </div>
 
